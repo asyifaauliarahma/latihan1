9a92ed0289f09e1a69c0af7517b95a03ffb0f790
asyifaauliarahma08 <asyifaauliarahmaasyifa@gmail.com>
2024-09-04 10:02:22 +0700

    login

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('LoginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+  
+    const correctUsername = "asyifa";
+    const correctPassword = "060608";
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+  
+  
+    if (username === correctUsername && password === correctPassword){
+      alert('Login Berhasil');
+      window.location.href = 'index.html';
+    }else{
+      alert('Username atau password salah')
+    }
+  });
